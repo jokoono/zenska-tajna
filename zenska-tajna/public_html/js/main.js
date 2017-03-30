39a386a923001203a7179d3a1c8a813cc288a106
jokoono <jovanajovsic@yahoo.com>
2017-03-30 16:01:35 +0200

    navigacija -nova

--- zenska-tajna/public_html/js/main.js
+++ zenska-tajna/public_html/js/main.js
@@ -1,7 +1,23 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+$(document).ready(function () {
+    var w = $(window).innerWidth();
+    $(window).resize(function () {
+        w = $(window).innerWidth();
 
-
+    });
+    $('.navbar .dropdown > a').click(function (e) {
+        e.preventDefault();
+        if (w < 768) {
+//            $('.dropdown-menu').slideUp();
+//            $(this).next('.dropdown-menu').slideToggle();
+            if($(this).next().hasClass('active')){
+                $(this).next().removeClass('active');
+                $(this).next().slideUp();
+            }else{
+                $('.dropdown-menu').removeClass('active');
+                $('.dropdown-menu').slideUp();
+                $(this).next('.dropdown-menu').addClass('active');
+                $(this).next('.dropdown-menu').slideDown();
+            }
+        }
+    });
+});
